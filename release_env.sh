fb8eb5b286f5eef8b7fdce0cac69ad055eccba5a
zy125442548 <125442548@qq.com>
2025-08-26 21:52:33 +0800

    first modify

--- release_env.sh
+++ release_env.sh
@@ -9,7 +9,7 @@ chmod 755 ${scriptdir}/report/create_summary
 #check if zUMIs environment has been unpacked from tar
 if [[ ! -d ${zumisenv} ]] || [[ ${zumisdir}/zUMIs-miniconda.partaa -nt ${zumisenv} ]] ; then
     [ -d ${zumisenv} ] || mkdir -p ${zumisenv}
-    cat `pwd`/zUMIs-miniconda.parta* > ${miniconda}
+    cat ${scriptdir}/zUMIs-miniconda.parta* > ${miniconda}
     tar -xj --overwrite -f ${miniconda} -C ${zumisenv}
 fi
 #activate zUMIs environment!
@@ -18,3 +18,6 @@ unset PYTHONPATH
 unset PYTHONHOME
 source ${zumisenv}/bin/activate
 conda-unpack
+
+chmod -R a+x ${scriptdir}/* 
+echo "change mode finish"
